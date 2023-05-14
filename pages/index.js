fd3cf78ecc62e66db56b0385e33951837a3f3daf
Максим <mgmakss@mail.ru>
2023-05-14 12:58:03 +0300

    fix: error

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,18 @@
+const title = document.querySelector('.title');
+
 const btnRu = document.querySelector('.header__lang-link_ru');
 const btnEn = document.querySelector('.header__lang-link_en');
-const title = document.querySelector('.lead__title');
+const leadTitle = document.querySelector('.lead__title');
+const leadSubtitle = document.querySelector('.lead__subtitle');
 
 btnRu.addEventListener('click', () => {
   title.textContent = 'Путешествия по России';
+  leadTitle.textContent = 'Путешествия по России';
+  leadSubtitle.textContent = 'Настоящая страна не в выпусках новостей, а здесь.';
 });
 
 btnEn.addEventListener('click', () => {
   title.textContent = 'Travel to Russia';
+  leadTitle.textContent = 'Travel to Russia';
+  leadSubtitle.textContent = 'The real country is not in the news releases, but here.';
 });
-
-/* select.addEventListener('change', changeURLLanguage);
-function changeURLLanguage () {
-  let lang = select.header__link-wrapper;
-  location.href = window.location.pathname + '#'+ lang;
-  location.reload();
-}
-
-function changeLanguage() {
-  let hash = window.location.hash;
-  if (!allLang.includes(hash)) {
-    location.href = window.location.pathname + '#en';
-    location.reload();
-  }
-  select.value = hash;
-  document.querySelector('title').innerHTML = langArr['unit'][hash];
-} */
